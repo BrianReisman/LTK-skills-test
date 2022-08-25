f320dd5f264a7dc267aa08fa7e59d2c34b614d9f
ltk-thomas <71238543+ltk-thomas@users.noreply.github.com>
2022-08-25 07:45:43 -0500

    Create BasicForm.js
    Signed-off-by: ltk-thomas <71238543+ltk-thomas@users.noreply.github.com>

---
+++ skills-test-react/src/BasicForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
+import { Button } from '@mui/material';
+
+const SimpleForm = () => (
+  <div>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={(values) => {}}
+    >
+      <Form>
+        <label htmlFor="todo">Last Name</label>
+        <Field id="todo" name="Add ToDo" placeholder="TextHere" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </div>
+);
+
+export default SimpleForm
