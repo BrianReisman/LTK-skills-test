9c4fe3ed184f17355d40a97a6846c2bf716ded57
ltk-thomas <71238543+ltk-thomas@users.noreply.github.com>
2022-08-25 07:45:11 -0500

    Update App.js
    Signed-off-by: ltk-thomas <71238543+ltk-thomas@users.noreply.github.com>

--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import AccountMenu from './AccountMenu';
 
-function App() {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+const BasicForm = () => {
+	return (
+		<React.Fragment>
+			<h1>Form Goes Here</h1>
+		</React.Fragment>
+	)
+};
+
+export default function App() {
+	const [openForm, setOpenForm] = React.useState(false);
+	const handleOnClick = (page) => {
+		console.log('open');
+		if(!openForm) { 
+			setOpenForm(true)
+		} else {
+			setOpenForm(false)
+		}
+	};
+  return (
+    <Container maxWidth="sm">
+	<AccountMenu onClick={handleOnClick} />
+	{openForm && <BasicForm />}
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          React Skills test
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
