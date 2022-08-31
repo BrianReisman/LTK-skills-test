b42bf3fec42ca92129fe2bbf6f628df7a5f7d0c5
BrianReisman <brianreisman+git@protonmail.com>
2022-08-30 20:02:20 -0400

    Comment out unused import and fix <BasicForm> typo

--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -2,24 +2,24 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import AccountMenu from './AccountMenu';
-import BasicForm from './BasicForm'
+import BasicForm from './BasicForm';
 
 export default function App() {
-	const [openForm, setOpenForm] = React.useState(false);
-	const handleOnClick = (page) => {
-		console.log('open');
-		if(!openForm) { 
-			setOpenForm(true)
-		} else {
-			setOpenForm(false)
-		}
-	};
+  const [openForm, setOpenForm] = React.useState(false);
+  const handleOnClick = (page) => {
+    console.log('open');
+    if (!openForm) {
+      setOpenForm(true);
+    } else {
+      setOpenForm(false);
+    }
+  };
   return (
     <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
+      <AccountMenu onClick={handleOnClick} />
+      {openForm && <BasicForm />}
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           React Skills test
